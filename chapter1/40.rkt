80c4536cfdb73a23558e130e3bffa1bda24d230c
Stephen Kraus <8003332+stephenmk@users.noreply.github.com>
2019-04-14 23:57:14 -0500

    Add exercise solutions 1.40 and 1.41

---
+++ chapter1/40.rkt
@@ -0,0 +1,48 @@
+#| Exercise 1.40.
+
+Define a procedure "cubic" that can be used together with the
+"newtons-method" procedure in expressions of the form
+
+(newtons-method (cubic a b c) 1)
+
+to approximate zeros of the cubic x^3 + ax^2 + bx + c. |#
+
+(require sicp)
+
+;; fixed-point definition from book
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+;; newtons-method definition from book
+(define (newtons-method f guess)
+  (define dx 0.00001)
+  (define (deriv g)
+    (lambda (x)
+      (/ (- (g (+ x dx)) (g x))
+         dx)))
+  (define (newton-transform g)
+    (lambda (x)
+      (- x (/ (g x) ((deriv g) x)))))
+  (fixed-point (newton-transform f) guess))
+
+;; cubic must accept three parameters and return a function
+;; x -> x^3 + ax^2 + bx + c
+(define (cubic a b c)
+  (define (square x) (* x x))
+  (define (cube x) (* x x x))
+  (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+
+;; A contrived example: a=2,b=2,c=1 has a zero at x = -1.
+;; x^3 + ax^2 + bx + c = (-1)^3 + 2(-1)^2 + 2(-1) + 1
+;;                     = -1 + 2 - 2 + 1
+;;                     = 0
+
+(newtons-method (cubic 2 2 1) 1) ; => -0.9999999999849342
---
+++ chapter1/41.rkt
@@ -0,0 +1,44 @@
+#| Exercise 1.41.
+
+Define a procedure "double" that takes a procedure of one argument as
+argument and returns a procedure that applies the original procedure
+twice. For example, if inc is a procedure that adds 1 to its argument,
+then (double inc) should be a procedure that adds 2. What value is
+returned by
+
+(((double (double double)) inc) 5)
+
+|#
+
+(require sicp)
+
+(define (double f)
+  (lambda (x) (f (f x))))
+
+((double inc) 5) ; => 7
+
+;; Applying the double transformation to itself, we get four
+;; applications total of the function argument.
+(define (quadruple f)
+  (lambda (x) (((double double) f) x)))
+
+((quadruple inc) 5) ; => 9
+
+;; For "double quadruple," we apply the quadruple procedure to itself
+;; twice. The first application of quadruple produces four
+;; applications of the function argument. With the second application
+;; of quadruple, these four function arguments are again quadrupled to
+;; produce 16 applications total.
+
+;; The name for the 16-tuple is
+;; sexdecuple. (https://en.wikipedia.org/wiki/Tuple)
+
+(define (sexdecuple f)
+  (lambda (x) (((double quadruple) f) x)))
+
+;; The following are equivalent
+(((double (double double)) inc) 5) ; => 21
+(((double quadruple) inc) 5) ; => 21
+((quadruple (quadruple inc)) 5) ; => 21
+((sexdecuple inc) 5) ; => 21
+(inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 5)))))))))))))))) ; => 21
