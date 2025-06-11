415a9fc51b9357ad63c01cf6498fcaad57c229c6
stephenmk <stephenmk@users.noreply.github.com>
2025-06-11 17:30:39 -0500

    Add solution for exercise 2.35

---
+++ chapter2/35.scm
@@ -0,0 +1,39 @@
+#| Exercise 2.35
+
+Redefine `count-leaves' from section 2.2.2 as an accumulation:
+
+    (define (count-leaves t)
+      (accumulate <??> <??> (map <??> <??>)))
+
+|#
+
+;;; Solution
+
+(define test-list '((1 2) 3 4 (5 6 (7 8))))
+
+;; My first attempt is not quite in the format specified by the
+;; problem description.
+(define (count-leaves t)
+  (cond ((null? t) 0)
+        ((not (pair? t)) 1)
+        (else (accumulate + 0 (map count-leaves t)))))
+
+(count-leaves test-list)  ;Value: 8
+
+
+;; It can be rewritten in the specified format, but it's very ugly.
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x) (cond ((null? x) 0) ((not (pair? x)) 1) (else (count-leaves x)))) t)))
+
+(count-leaves test-list)  ;Value: 8
+
+
+;; Maybe it's okay to write it like this.
+(define (count-leaves t)
+  (define (op x)
+    (cond ((null? x) 0)
+          ((not (pair? x)) 1)
+          (else (count-leaves x))))
+  (accumulate + 0 (map op t)))
+
+(count-leaves test-list)  ;Value: 8
