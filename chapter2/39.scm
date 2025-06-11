6269fefa4d10a5ff2a5ff60dade982173a67646e
stephenmk <stephenmk@users.noreply.github.com>
2025-06-11 18:55:35 -0500

    Compatibility with mit-scheme

--- chapter2/39.scm
+++ chapter2/39.scm
@@ -13,15 +13,30 @@ terms of `fold-right' and `fold-left' from Exercise 2-38
 
 ;;; Solution
 
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
 (define (reverse sequence)
-  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
 
 (reverse '(1 2 3 4))
 ;;Value: (4 3 2 1)
 
 
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
 (define (reverse sequence)
-  (fold-left (lambda (x y) (cons y x)) nil sequence))
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
 
 (reverse '(1 2 3 4))
 ;;Value: (4 3 2 1)
