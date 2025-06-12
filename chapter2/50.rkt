e34de57eb0bbdc9daf94b5a67446b4ab8c0566bd
stephenmk <stephenmk@users.noreply.github.com>
2025-06-12 14:42:25 -0500

    Add descriptions to exercise 2.50

--- chapter2/50.rkt
+++ chapter2/50.rkt
@@ -8,6 +8,8 @@ horizontally, and transformations that rotate painters
 counterclockwise by 180 degrees and 270 degrees. |#
 
 ;;; Solution
+(display "Original")
+(newline)
 (paint einstein)
 
 (define (flip-horiz painter)
@@ -16,6 +18,8 @@ counterclockwise by 180 degrees and 270 degrees. |#
                      (make-vect 0.0 0.0)   ; new end of `edge1'
                      (make-vect 1.0 1.0))) ; new end of `edge2'
 
+(display "Horizontally flipped")
+(newline)
 (paint (flip-horiz einstein))
 
 (define (rotate-ccw-180 painter)
@@ -24,6 +28,8 @@ counterclockwise by 180 degrees and 270 degrees. |#
                      (make-vect 0.0 1.0)   ; new end of `edge1'
                      (make-vect 1.0 0.0))) ; new end of `edge2'
 
+(display "Original rotated 180 degrees counter-clockwise")
+(newline)
 (paint (rotate-ccw-180 einstein))
 
 (define (rotate-ccw-270 painter)
@@ -32,4 +38,13 @@ counterclockwise by 180 degrees and 270 degrees. |#
                      (make-vect 0.0 0.0)   ; new end of `edge1'
                      (make-vect 1.0 1.0))) ; new end of `edge2'
 
+(display "Original rotated 270 degrees counter-clockwise")
+(newline)
 (paint (rotate-ccw-270 einstein))
+
+(display "Four 270 rotations = Three 360 rotations = back to original")
+(newline)
+(paint (rotate-ccw-270
+        (rotate-ccw-270
+         (rotate-ccw-270
+          (rotate-ccw-270 einstein)))))
